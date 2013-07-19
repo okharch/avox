001ce6cac6643c4a324c54c21d8ff3770443a9b7
Oleksandr Kharchenko @ centos <okharch@gmail.com>
2013-07-19 06:23:08 +0200

    comment on clerks tables

---
+++ database/clerks-comments.sql
@@ -0,0 +1,37 @@
+COMMENT ON TABLE clerk_type IS 'This is reference table for clerk_id value. clerk_id => name';
+accounts               | table    | rink_dba | 0 bytes    |
+ public | accounts_log           | table    | rink_dba | 0 bytes    |
+ public | clerk_status           | table    | rink_dba | 16 kB      |
+ public | clerk_type             | table    | rink_dba | 16 kB      | This is reference table for clerk_id value. clerk_id => name
+ public | clerks                 | table    | rink_dba | 8192 bytes |
+ public | clerks_log             | table    | rink_dba | 8192 bytes |
+ public | client_status          | table    | rink_dba | 16 kB      |
+ public | client_type            | table    | rink_dba | 16 kB      |
+ public | clients                | table    | rink_dba | 8192 bytes |
+ public | clients_log            | table    | rink_dba | 8192 bytes |
+ public | contract_status        | table    | rink_dba | 8192 bytes |
+ public | contracts              | table    | rink_dba | 8192 bytes |
+ public | contracts_log          | table    | rink_dba | 8192 bytes |
+ public | currency_rate          | table    | rink_dba | 0 bytes    |
+ public | currency_type          | table    | rink_dba | 16 kB      |
+ public | msg_type               | table    | rink_dba | 8192 bytes |
+ public | problem_status         | table    | rink_dba | 8192 bytes |
+ public | problem_type           | table    | rink_dba | 8192 bytes |
+ public | problems               | table    | rink_dba | 8192 bytes |
+problems_log           | table    | rink_dba | 8192 bytes |
+ public | service_status         | table    | rink_dba | 16 kB      |
+ public | service_type           | table    | rink_dba | 16 kB      |
+ public | services               | table    | rink_dba | 8192 bytes |
+ public | services_log           | table    | rink_dba | 8192 bytes |
+ public | services_tariff        | table    | rink_dba | 0 bytes    |
+ public | services_tariff_log    | table    | rink_dba | 0 bytes    |
+ public | sysmsg                 | table    | rink_dba | 8192 bytes |
+ public | sysmsg_type            | table    | rink_dba | 8192 bytes |
+ public | tariff                 | table    | rink_dba | 0 bytes    |
+ public | tariff_day             | table    | rink_dba | 0 bytes    |
+ public | tariff_day_log         | table    | rink_dba | 0 bytes    |
+ public | tariff_plan            | table    | rink_dba | 8192 bytes |
+ public | tariff_plan_log        | table    | rink_dba | 8192 bytes |
+ public | tariff_time            | table    | rink_dba | 0 bytes    |
+ public | tariff_time_log        | table    | rink_dba | 0 bytes    |
+ public | tariff_type            | table    | rink_dba | 8192 bytes |
